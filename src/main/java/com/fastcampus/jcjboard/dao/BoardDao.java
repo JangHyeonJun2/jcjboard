bc184bb756111d04d6b9c47879d5f1c6bc0cbe3d
Jaeyong <koola97620@nate.com>
2018-10-08 14:20:02 +0900

    BoardDAO 에 getBoardList() 생성, DBUtil 생성. /board/list 페이지 출력.

--- src/main/java/com/fastcampus/jcjboard/dao/BoardDao.java
+++ src/main/java/com/fastcampus/jcjboard/dao/BoardDao.java
@@ -1,5 +1,54 @@
 package com.fastcampus.jcjboard.dao;
 
+import com.fastcampus.jcjboard.servlet.BoardDO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BoardDao {
-    private String test;
+    private String dbUrl="jdbc:mysql://localhost:3306/jcjboard?useSSL=false&serverTimezone=UTC&useUnicode=true&characterEncoding=UTF-8";
+    private String dbId = "root";
+    private String dbPassword = "1234";
+
+    public List<BoardDO> getBoardList() {
+        List<BoardDO> list = new ArrayList<>();
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+
+            conn = DbUtil.connect(dbUrl,dbId,dbPassword);
+            String sql ="select boardid,nickname,title,content from board";
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+
+            while(rs.next()) {
+                BoardDO board = new BoardDO();
+                board.setId(rs.getInt(1));
+                board.setNickname(rs.getString(2));
+                board.setTitle(rs.getString(3));
+                board.setContent(rs.getString(4));
+                list.add(board);
+                System.out.println(board.getId() + board.getContent());
+            }
+
+
+        }catch(Exception ex) {
+            ex.printStackTrace();
+        }finally {
+            DbUtil.close(conn,ps,rs);
+        }
+
+        return list;
+    }
+
+
+
+
+
+
 }
---
+++ webapp/WEB-INF/views/list.jsp
@@ -0,0 +1,24 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: choijaeyong
+  Date: 08/10/2018
+  Time: 1:38 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+<h1> List </h1>
+
+
+게시판 글이 몇개 있니???
+${ boardSize } <br>
+
+
+</body>
+</html>
